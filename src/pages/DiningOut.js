cf30ad2d2aab10bb6b06ab522d15c2f46f62cb26
code_pitam <pitamdas462@gmail.com>
2022-12-17 15:34:58 +0530

    j

--- src/pages/DiningOut.js
+++ src/pages/DiningOut.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import Foodcart from '../components/Foodcart'
 
 const DiningOut = () => {
   return (
-    <div className='dinignout'>DiningOut</div>
+    <div className='dinignout'>
+      <div className="menu">
+      <Foodcart price={'2000 for two'} des={'Continental, Chaines'} name={'Botanik'} src={'https://b.zmtcdn.com/data/pictures/0/20435900/7814fe8619b6cf1d02316c511e5edbd6_featured_v2.jpg'}/>
+      <Foodcart price={'2000 for two'} des={'Continental, Chaines'} name={'Botanik'} src={'https://b.zmtcdn.com/data/pictures/0/20435900/7814fe8619b6cf1d02316c511e5edbd6_featured_v2.jpg'}/>
+      <Foodcart price={'2000 for two'} des={'Continental, Chaines'} name={'Botanik'} src={'https://b.zmtcdn.com/data/pictures/0/20435900/7814fe8619b6cf1d02316c511e5edbd6_featured_v2.jpg'}/>
+      <Foodcart price={'2000 for two'} des={'Continental, Chaines'} name={'Botanik'} src={'https://b.zmtcdn.com/data/pictures/0/20435900/7814fe8619b6cf1d02316c511e5edbd6_featured_v2.jpg'}/>
+      <Foodcart price={'2000 for two'} des={'Continental, Chaines'} name={'Botanik'} src={'https://b.zmtcdn.com/data/pictures/0/20435900/7814fe8619b6cf1d02316c511e5edbd6_featured_v2.jpg'}/>
+      <Foodcart price={'2000 for two'} des={'Continental, Chaines'} name={'Botanik'} src={'https://b.zmtcdn.com/data/pictures/0/20435900/7814fe8619b6cf1d02316c511e5edbd6_featured_v2.jpg'}/>
+      <Foodcart price={'2000 for two'} des={'Continental, Chaines'} name={'Botanik'} src={'https://b.zmtcdn.com/data/pictures/0/20435900/7814fe8619b6cf1d02316c511e5edbd6_featured_v2.jpg'}/>
+
+  
+
+      </div>
+    </div>
   )
 }
 
--- src/pages/Nightlife.js
+++ src/pages/Nightlife.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import Foodcart from '../components/Foodcart'
 
 const Nightlife = () => {
   return (
-    <div className='nightlife'>Nightlife</div>
+    <div className='nightlife'> <div className="menu">
+    <Foodcart price={'2000 for two'} des={'Continental, Chaines'} name={'Raasta'} src={'https://b.zmtcdn.com/data/pictures/5/20109615/51be5d82b855b21aec13641de4ab3057_featured_v2.jpg'}/>
+    <Foodcart price={'2000 for two'} des={'Continental, Chaines'} name={'Raasta'} src={'https://b.zmtcdn.com/data/pictures/5/20109615/51be5d82b855b21aec13641de4ab3057_featured_v2.jpg'}/>
+    <Foodcart price={'2000 for two'} des={'Continental, Chaines'} name={'Raasta'} src={'https://b.zmtcdn.com/data/pictures/5/20109615/51be5d82b855b21aec13641de4ab3057_featured_v2.jpg'}/>
+    <Foodcart price={'2000 for two'} des={'Continental, Chaines'} name={'Raasta'} src={'https://b.zmtcdn.com/data/pictures/5/20109615/51be5d82b855b21aec13641de4ab3057_featured_v2.jpg'}/>
+    <Foodcart price={'2000 for two'} des={'Continental, Chaines'} name={'Raasta'} src={'https://b.zmtcdn.com/data/pictures/5/20109615/51be5d82b855b21aec13641de4ab3057_featured_v2.jpg'}/>
+    <Foodcart price={'2000 for two'} des={'Continental, Chaines'} name={'Raasta'} src={'https://b.zmtcdn.com/data/pictures/5/20109615/51be5d82b855b21aec13641de4ab3057_featured_v2.jpg'}/>
+    <Foodcart price={'2000 for two'} des={'Continental, Chaines'} name={'Raasta'} src={'https://b.zmtcdn.com/data/pictures/5/20109615/51be5d82b855b21aec13641de4ab3057_featured_v2.jpg'}/>
+    <Foodcart price={'2000 for two'} des={'Continental, Chaines'} name={'Raasta'} src={'https://b.zmtcdn.com/data/pictures/5/20109615/51be5d82b855b21aec13641de4ab3057_featured_v2.jpg'}/>
+    <Foodcart price={'2000 for two'} des={'Continental, Chaines'} name={'Raasta'} src={'https://b.zmtcdn.com/data/pictures/5/20109615/51be5d82b855b21aec13641de4ab3057_featured_v2.jpg'}/>
+
+
+    </div></div>
   )
 }
 
